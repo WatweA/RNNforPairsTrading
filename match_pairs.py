12e60ea7904df8ec2014eb89aab3f562a220a1c7
Aaditya Watwe <45186715+WatweA@users.noreply.github.com>
2021-03-31 14:31:34 -0400

    changed string pair key to list pair key

--- match_pairs.py
+++ match_pairs.py
@@ -106,10 +106,9 @@ def match_tickers(corr_dict: Dict[str, Dict[str, float]], min_corr=0, debug=Fals
 matched_tickers = match_tickers(top_5_corr, min_corr=0.75)
 # tuple to tuple string representation for keys, save as JSON
 matched_tickers_JSON = json.dumps(
-    {str(k): v for k,v in matched_tickers.items()}, 
+    {list(k): v for k,v in matched_tickers.items()}, 
     sort_keys=False, indent=4, separators=(',', ': '))
 save_path = "data/info/pairs.json"
 open(save_path,"w").write(matched_tickers_JSON)
 print(matched_tickers)
-
 
