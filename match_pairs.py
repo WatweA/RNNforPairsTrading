0d2e1ae1c8a8353db3b24ef5ef7aba19d5226a0c
Aaditya Watwe <45186715+WatweA@users.noreply.github.com>
2021-03-31 14:32:05 -0400

    comment fix

--- match_pairs.py
+++ match_pairs.py
@@ -104,7 +104,7 @@ def match_tickers(corr_dict: Dict[str, Dict[str, float]], min_corr=0, debug=Fals
 
 # get the matched tickers for all with correlation coefficients above 0.75
 matched_tickers = match_tickers(top_5_corr, min_corr=0.75)
-# tuple to tuple string representation for keys, save as JSON
+# tuple to list representation for keys, save as JSON
 matched_tickers_JSON = json.dumps(
     {list(k): v for k,v in matched_tickers.items()}, 
     sort_keys=False, indent=4, separators=(',', ': '))
