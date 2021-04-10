cbb7feecde3301c0991f8f6a0d92cb8e7be70e5f
Aaditya Watwe <45186715+WatweA@users.noreply.github.com>
2021-04-09 21:33:38 -0400

    GBM simulations for all pairs complete

--- src/generate_data.py
+++ src/generate_data.py
@@ -2,16 +2,9 @@
 
 import pandas as pd
 import json
+from utils.preprocessing import generate_pair_data
 
 pairs = {eval(k): v for k,v in dict(json.loads(open("data/info/pairs.json", "r").read())).items()}
-
-pair = ('ADX', 'TY')
-df_A = pd.read_pickle("data/raw/" + pair[0] + ".zip")  # needs pickle5 compression (python 3.8)
-df_B = pd.read_pickle("data/raw/" + pair[1] + ".zip")  # needs pickle5 compression (python 3.8)
-
-pair_df = generate_pair_data(df_A, df_B, "1999-01-01", "2021-03-01")
-
-pair_df.loc["2000-01-01":, :]
 
 for pair in pairs.keys():
     df_A = pd.read_pickle("data/raw/" + pair[0] + ".zip")  # needs pickle5 compression (python 3.8)
